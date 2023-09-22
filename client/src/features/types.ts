f2e9cb2a81c154e0bfe49d1af91bacfd89826f64
GulshanSS <sahugulshan686@gmail.com>
2023-09-23 01:34:46 +0530

    Added reactions and user types

--- client/src/features/types.ts
+++ client/src/features/types.ts
@@ -2,4 +2,17 @@ export type TPost = {
   id: string;
   title: string;
   description: string;
+  date: string;
+  userId: string;
+  reactions: TReactions;
+};
+
+export type TReactions = {
+  thumbsUp: number;
+  thumbsDown: number;
+};
+
+export type TUser = {
+  id: string;
+  name: string;
 };
