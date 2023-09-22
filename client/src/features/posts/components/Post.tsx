2f0edb0df4333963f3e37fa8f86b2bdbff48b3b0
GulshanSS <sahugulshan686@gmail.com>
2023-09-23 01:36:45 +0530

    Added new properties to diplay in post

--- client/src/features/posts/components/Post.tsx
+++ client/src/features/posts/components/Post.tsx
@@ -2,25 +2,36 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import DisplayUserName from "@/features/users/components/DisplayUserName";
+import DisplayTime from "./DisplayTime";
+import IconReactions from "./IconReactions";
+import { TPost } from "@/features/types";
 
 type Props = {
-  title: string;
-  description: string;
+  post: TPost;
 };
 
-const Post = ({ title, description }: Props) => {
+const Post = ({ post }: Props) => {
   return (
     <div className="w-96">
       <Card>
         <CardHeader>
-          <CardTitle>{title}</CardTitle>
+          <CardTitle>{post.title}</CardTitle>
         </CardHeader>
         <CardContent>
-          <CardDescription>{description}</CardDescription>
+          <CardDescription>{post.description}</CardDescription>
         </CardContent>
+        <CardFooter>
+          <div className="flex flex-col space-y-2">
+            <DisplayUserName userId={post.userId} />
+            <DisplayTime timestamp={post.date} />
+            <IconReactions post={post} />
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
