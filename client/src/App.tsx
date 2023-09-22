9186414c40660a076605f85ad5fe9fa65db3bea9
GulshanSS <sahugulshan686@gmail.com>
2023-09-22 19:22:49 +0530

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Dev-Log</h1>
+      <Button variant="default" size="lg">
+        Click Me!!
+      </Button>
+    </div>
+  );
+};
+
+export default App;
