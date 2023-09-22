b608994545cb726696b58d3be63a15ab6a9df697
GulshanSS <sahugulshan686@gmail.com>
2023-09-23 01:35:28 +0530

    Added addReact func and date | userId | reaction to post

--- client/src/features/posts/postsSlice.ts
+++ client/src/features/posts/postsSlice.ts
@@ -1,9 +1,30 @@
+import { sub } from "date-fns";
 import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
 import { TPost } from "../types";
 
 const initialState: TPost[] = [
-  { id: "1", title: "This is 1st Title", description: "This is 1st Content" },
-  { id: "2", title: "This is 2nd Title", description: "This is 2nd Content" },
+  {
+    id: "1",
+    title: "This is 1st Title",
+    description: "This is 1st Content",
+    userId: "1",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      thumbsDown: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "This is 2nd Title",
+    description: "This is 2nd Content",
+    userId: "2",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      thumbsDown: 0,
+    },
+  },
 ];
 
 const postsSlice = createSlice({
@@ -14,21 +35,40 @@ const postsSlice = createSlice({
       reducer: (state, action: PayloadAction<TPost>) => {
         state.push(action.payload);
       },
-      prepare: (title: string, description: string) => {
+      prepare: (title: string, description: string, userId: string) => {
         return {
           payload: {
             id: nanoid(),
             title,
             description,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              thumbsDown: 0,
+            },
           },
         };
       },
+    },
+    addReaction: (
+      state,
+      action: PayloadAction<{
+        postId: string;
+        reaction: "thumbsUp" | "thumbsDown";
+      }>
+    ) => {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state: { posts: TPost[] }) => state.posts;
 
-export const { addPost } = postsSlice.actions;
+export const { addPost, addReaction } = postsSlice.actions;
 
 export default postsSlice.reducer;
